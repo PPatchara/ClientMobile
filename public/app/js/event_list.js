086940fe8021fe9793d84e103717e983010d7667
PATCHARA PATTANAKIMHUN <Justwyne@PATCHARAs-Air.it.kmitl.ac.th>
2017-04-09 21:09:37 +0700

    add data dummies

---
+++ public/app/js/event_list.js
@@ -0,0 +1,107 @@
+var event_list = [
+      {
+      "id": "#001",
+      "category": "Competition",
+      "image": "1.jpg",
+      "title": "Test1",
+      "description": "Description1",
+      "schedule": [
+            {
+                  "topic":"Register",
+                  "date":"01/01/2017",
+                  "time":"9:00-12:00"
+            }
+      ],
+      "location": "A1 Building",
+      "register": "www.google.com",                                                                                                                                                                                           
+      "calendar": "event001.ics",
+      "contact": [
+            {
+                  "website": "www.google.com",
+                  "email": "test1@gmail.com",
+                  "tel": ["089-7046621,(Wyne)","0826588017,(Non)"]
+            }
+      ],
+      "share": [
+            {
+                  "facebook": "https://www.facebook.com/sharer/sharer.php?u=http%3A//framework7.io/docs/cards.html&display=popup",
+                  "email": [
+                        {
+                              "subject": "Test1",
+                              "body": "Description1"
+                        }
+                  ]
+            }
+      ]
+      },
+      {
+      "id": "#002",
+      "category": "Camp",
+      "image": "2.jpg",
+      "title": "Test2",
+      "description": "Description2",
+      "schedule": [
+            {
+                  "topic":"Register",
+                  "date":"02/02/2017",
+                  "time":"9:00-12:00"
+            }
+      ],
+      "location": "A2 Building",
+      "register": "www.google.com",                                                                                                                                                                                           
+      "calendar": "event002.ics",
+      "contact": [
+            {
+                  "website": "www.google.com",
+                  "email": "test2@gmail.com",
+                  "tel": ["089-7046621,(Wyne)","0826588017,(Non)"]
+            }
+      ],
+      "share": [
+            {
+                  "facebook": "https://www.facebook.com/sharer/sharer.php?u=http%3A//framework7.io/docs/cards.html&display=popup",
+                  "email": [
+                        {
+                              "subject": "Test2",
+                              "body": "Description2"
+                        }
+                  ]
+            }
+      ]
+      },
+      {
+      "id": "#003",
+      "category": "Keynote",
+      "image": "3.jpg",
+      "title": "Test3",
+      "description": "Description3",
+      "schedule": [
+            {
+                  "topic":"Register",
+                  "date":"03/03/2017",
+                  "time":"9:00-12:00"
+            }
+      ],
+      "location": "A3 Building",
+      "register": "www.google.com",                                                                                                                                                                                           
+      "calendar": "event003.ics",
+      "contact": [
+            {
+                  "website": "www.google.com",
+                  "email": "test3@gmail.com",
+                  "tel": ["089-7046621,(Wyne)","0826588017,(Non)"]
+            }
+      ],
+      "share": [
+            {
+                  "facebook": "https://www.facebook.com/sharer/sharer.php?u=http%3A//framework7.io/docs/cards.html&display=popup",
+                  "email": [
+                        {
+                              "subject": "Test3",
+                              "body": "Description3"
+                        }
+                  ]
+            }
+      ]
+      }
+]
