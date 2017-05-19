c422ddcad4daa494de1c4c91cf9b52f96a1cd727
PATCHARA PATTANAKIMHUN <Justwyne@PATCHARAs-Air.it.kmitl.ac.th>
2017-05-19 12:21:53 +0700

    Add data dummies

--- public/app/js/event_list.js
+++ public/app/js/event_list.js
@@ -7,134 +7,249 @@ var event_list = [
                   "color": "#ff9500"
             },
       "image": "1.jpg",
-      "title": "Test1",
-      "description": "Description1",
+      "title": "FinTech Startup @ KMITL",
+      "description": 'คณะเทคโนโลยีสารสนเทศ ได้ร่วมกับบริษัทหลักทรัพย์ ฟินันเซีย ไซรัส จำกัด (มหาชน) จัดการแข่งขันโครงการ FinTech Startup ชิงเงินรางวัลรวม 50,000 บาท และสามารถเข้าร่วมอบรมฟรี 2 วัน ในหัวข้อ ความรู้ด้านการลงทุนกับหุ้นออนไลน์ และหัวข้อการสร้างนวัตกรรมทางการเงินเพื่อการแข่งขัน',
       "schedule": [
             {
-                  "topic":"Register",
-                  "date":"01/01/2017",
-                  "time":"9:00-12:00"
+                  "topic":"วันเปิดสมัคร",
+                  "date":"ตั้งแต่วันนี้ - 15 ก.พ. 2560",
+                  "time":""
             }
       ],
-      "location": "A1 Building",
+      "location": "",
       "register": "",                                                                                                                                                                                           
-      "calendar": "event001.ics",
+      "calendar": "",
       "contact": 
             {
-                  "website": "http://www.google.com",
-                  "email": "test1@gmail.com",
+                  "website": "https://goo.gl/rvNK0y",
+                  "email": "",
                   "tel": [
                         {
-                              "name": "Wyne",
-                              "number": "0897046621"
-                        },
-                        {
-                              "name": "Non",
-                              "number": "0826588017"
+                              "name": "",
+                              "number": "02-658-9829"
                         }
                   ]
             },
       "share": {
-                  "facebook": "https://www.facebook.com/sharer/sharer.php?u=http%3A//framework7.io/docs/cards.html&display=popup",
-                  "googleplus": "https://plus.google.com/share?url=https://framework7.io",
-                  "twitter": "https://twitter.com/intent/tweet?url=https://framework7.io",
+                  "facebook": "https://www.facebook.com/sharer/sharer.php?u=https://goo.gl/rvNK0y&display=popup",
+                  "googleplus": "https://plus.google.com/share?url=https://goo.gl/rvNK0y",
+                  "line": "http://line.me/R/msg/text/?https://goo.gl/rvNK0y",
+                  "twitter": "https://twitter.com/intent/tweet?url=https://goo.gl/rvNK0y",
                   "email": {
-                        "subject": "Test1",
-                        "body": "Description1"
+                        "subject": "FinTech Startup @ KMITL",
+                        "body": "https://goo.gl/rvNK0y"
                   }
             }
       },
       {
       "id": "#002",
-      "category": 
+      "category":
             {
-                  "type": "Camp",
-                  "color": "#ff2d55"
+                  "type": "Competition",
+                  "color": "#ff9500"
             },
       "image": "2.jpg",
-      "title": "Test2",
-      "description": "Description2",
+      "title": 'ประกวดออกแบบ Infographic และ Clip VDO ในโครงการ "ทำดีเพื่อพ่อ สานต่อแก้ปัญหายาเสพติด"',
+      "description": 'สำนักงานคณะกรรมการป้องกันและปราบปรามยาเสพติด กระทรวงยุติธรรม ขอเชิญเยาวชนอายุระหว่าง 15-18 ปีและประชาชนทั่วไป อายุ 19 ปีขึ้นไป ส่งผลงานเข้าร่วมการประกวดออกแบบ Infographic และ Clip VDO (ความยาว 1 นาที) ในโครงการ "ทำดีเพื่อพ่อ สานต่อแก้ปัญหายาเสพติด" ภายใต้หัวข้อ "ผู้เสพ คือ ผู้ป่วย" เพื่อรณรงค์เชิญชวนให้ผู้เสพ ผู้ติดยาเสพติดเข้ารับการบำบัดรักษา ชิงเงินรางวัลมูลค่ารวมกว่า 360,000 บาท พร้อมโล่รางวัล และใบประกาศนียบัตร',
       "schedule": [
             {
-                  "topic":"Register",
-                  "date":"02/02/2017",
-                  "time":"9:00-12:00"
+                  "topic":"วันเปิดสมัคร",
+                  "date":"ตั้งแต่วันนี้ - 15 มิ.ย. 2560",
+                  "time":""
             }
       ],
-      "location": "A2 Building",
+      "location": "",
       "register": "",                                                                                                                                                                                           
-      "calendar": "event002.ics",
-      "contact":
+      "calendar": "",
+      "contact": 
             {
-                  "website": "http://www.google.com",
-                  "email": "test2@gmail.com",
+                  "website": "https://goo.gl/ZT4wJp",
+                  "email": "",
                   "tel": [
                         {
-                              "name": "Wyne",
-                              "number": "0897046621"
-                        },
-                        {
-                              "name": "Non",
-                              "number": "0826588017"
+                              "name": "",
+                              "number": "09-5486-0303"
                         }
                   ]
             },
       "share": {
-                  "facebook": "https://www.facebook.com/sharer/sharer.php?u=http%3A//framework7.io/docs/cards.html&display=popup",
-                  "googleplus": "https://plus.google.com/share?url=https://framework7.io",
-                  "twitter": "https://twitter.com/intent/tweet?url=https://framework7.io",
+                  "facebook": "https://goo.gl/ZT4wJp&display=popup",
+                  "googleplus": "https://plus.google.com/share?url=https://goo.gl/ZT4wJp",
+                  "line": "http://line.me/R/msg/text/?https://goo.gl/ZT4wJp",
+                  "twitter": "https://twitter.com/intent/tweet?url=https://goo.gl/ZT4wJp",
                   "email": {
-                        "subject": "Test2",
-                        "body": "Description2"
+                        "subject": 'ประกวดออกแบบ Infographic และ Clip VDO ในโครงการ "ทำดีเพื่อพ่อ สานต่อแก้ปัญหายาเสพติด"',
+                        "body": "https://goo.gl/ZT4wJp"
                   }
             }
-      
       },
       {
       "id": "#003",
       "category":
             {
-                  "type": "Keynote",
-                  "color": "#4cd964"
+                  "type": "Competition",
+                  "color": "#ff9500"
             },
       "image": "3.jpg",
-      "title": "Test3",
-      "description": "Description3",
+      "title": 'ร่วมประลองไอเดีย ประกวดออกแบบ Mascot สัญลักษณ์ สจล.',
+      "description": 'มาแล้ว กับกิจกรรมเด็ดๆ ร่วมประลองไอเดีย เฟ้นหาสุดยอดฝีมือการออกแบบ Mascot สัญลักษณ์ สจล. ชิงเงินรางวัลรวมกว่า 60,000 บาท นักเรียน นักศึกษา ศิษย์เก่า สจล.และประชาชนทั่วไป สามารถส่งผลงานเข้าร่วมประกวดได้ทุกท่าน ไม่จำกัดสถาบันการศึกษา ไม่จำกัดอายุ และไม่จำกัดภูมิลำเนา',
       "schedule": [
             {
-                  "topic":"Register1",
-                  "date":"03/03/2017",
-                  "time":"9:00-12:00"
+                  "topic":"วันเปิดสมัคร",
+                  "date":"ตั้งแต่วันที่ 15 พฤษภาคม ถึง วันที่ 15 กรกฎาคม 2560",
+                  "time":""
             }
       ],
-      "location": "A3 Building",
-      "register": "https://goo.gl/forms/UW89mWUsE1isBObG3",                                                                                                                                                                                           
-      "calendar": "event003.ics",
+      "location": "",
+      "register": "",                                                                                                                                                                                           
+      "calendar": "",
       "contact": 
             {
-                  "website": "http://www.google.com",
-                  "email": "test3@gmail.com",
+                  "website": "https://goo.gl/WJy1IG",
+                  "email": "pr.kmitl@kmitl.ac.th",
                   "tel": [
                         {
-                              "name": "Wyne",
-                              "number": "0897046621"
-                        },
-                        {
-                              "name": "Non",
-                              "number": "0826588017"
+                              "name": "",
+                              "number": "02-329-8111"
                         }
                   ]
             },
       "share": {
-                  "facebook": "https://www.facebook.com/sharer/sharer.php?u=http%3A//framework7.io/docs/cards.html&display=popup",
-                  "googleplus": "https://plus.google.com/share?url=https://framework7.io",
-                  "twitter": "https://twitter.com/intent/tweet?url=https://framework7.io",
+                  "facebook": "https://www.facebook.com/sharer/sharer.php?u=https://goo.gl/WJy1IG&display=popup",
+                  "googleplus": "https://plus.google.com/share?url=https://goo.gl/WJy1IG",
+                  "line": "http://line.me/R/msg/text/?https://goo.gl/WJy1IG",
+                  "twitter": "https://twitter.com/intent/tweet?url=https://goo.gl/WJy1IG",
                   "email": {
-                        "subject": "Test3",
-                        "body": "Description3"
+                        "subject": 'ร่วมประลองไอเดีย ประกวดออกแบบ Mascot สัญลักษณ์ สจล.',
+                        "body": "https://goo.gl/WJy1IG"
                   }
             }
-      
+      },
+      {
+      "id": "#004",
+      "category":
+            {
+                  "type": "Competition",
+                  "color": "#ff9500"
+            },
+      "image": "4.jpg",
+      "title": 'HACKATHON @ KMITL',
+      "description": 'HACKATHON เป็นโครงการที่สนับสนุนและผลักดันให้น้องๆนักศึกษารวมกลุ่มกันนำ แนวคิด แรงบันดาลใจหรือสิ่งชอบ มาสร้างสรรค์ให้เกิดเป็นผลงานและแผนธุรกิจ ที่สามารถตอบโจทย์ความต้องการของสังคมในปัจจุบัน และออกขายสู่ตลาดได้จริงภายใต้หัวข้อ Smart Living',
+      "schedule": [
+            {
+                  "topic":"วันเปิดสมัคร",
+                  "date":"วันที่ 9-11 มีนาคม 2560",
+                  "time":""
+            }
+      ],
+      "location": "",
+      "register": "https://docs.google.com/…/1FAIpQLSeeLFucMxrMolBL5L…/viewform",                                                                                                                                                                                           
+      "calendar": "",
+      "contact": 
+            {
+                  "website": "https://goo.gl/WPXP1p",
+                  "email": "",
+                  "tel": [
+                        {
+                              "name": "P'แตงโม",
+                              "number": "083-883-5622"
+                        }
+                  ]
+            },
+      "share": {
+                  "facebook": "https://www.facebook.com/sharer/sharer.php?u=https://goo.gl/WPXP1p&display=popup",
+                  "googleplus": "https://plus.google.com/share?url=https://goo.gl/WPXP1p",
+                  "line": "http://line.me/R/msg/text/?https://goo.gl/WPXP1p",
+                  "twitter": "https://twitter.com/intent/tweet?url=https://goo.gl/WPXP1p",
+                  "email": {
+                        "subject": 'HACKATHON @ KMITL',
+                        "body": "https://goo.gl/WPXP1p"
+                  }
+            }
+      },
+      {
+      "id": "#005",
+      "category":
+            {
+                  "type": "Entertainment",
+                  "color": "#007aff"
+            },
+      "image": "5.jpg",
+      "title": 'SNAKE SNAKE FISH FISH',
+      "description": 'TALK SHOW จากวิทยากรที่คุณไม่ควรพลาด พี่เอ้ , อดัม แบรดชอว์ , คริสโตเฟอร์ ไรท์ และความสนุกทางภาษากำลังจะเกิดขึ้นไปกับเหล่าศิลปินสุดแสบ สิงโต นำโชค และ ROOM39',
+      "schedule": [
+            {
+                  "topic":"",
+                  "date":"วันที่ 25 เมษายน 2560",
+                  "time":"17.00 - 22.00 น."
+            }
+      ],
+      "location": "หอประชุมใหญ่คณะวิศวกรรมศาสตร์ สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง (KMITL)",
+      "register": "",                                                                                                                                                                                           
+      "calendar": "",
+      "contact": 
+            {
+                  "website": "https://goo.gl/UJF140",
+                  "email": "",
+                  "tel": [
+                        {
+                              "name": "P'แตงโม",
+                              "number": "083-883-5622"
+                        }
+                  ]
+            },
+      "share": {
+                  "facebook": "https://www.facebook.com/sharer/sharer.php?u=https://goo.gl/UJF140&display=popup",
+                  "googleplus": "https://plus.google.com/share?url=https://goo.gl/UJF140",
+                  "line": "http://line.me/R/msg/text/?https://goo.gl/UJF140",
+                  "twitter": "https://twitter.com/intent/tweet?url=https://goo.gl/UJF140",
+                  "email": {
+                        "subject": 'SNAKE SNAKE FISH FISH',
+                        "body": "https://goo.gl/UJF140"
+                  }
+            }
+      },
+      {
+      "id": "#006",
+      "category":
+            {
+                  "type": "Job / Internship",
+                  "color": "#4cd964"
+            },
+      "image": "6.jpg",
+      "title": 'CE Smart Career',
+      "description": 'เตรียมพบกับ IT Job Fair ที่ใหญ่ที่สุดใน KMITL ประจำปี 2560 นั่นก็คือ “CE Smart Career ”!! ใครที่กำลังมองหาบริษัทเพื่อสมัครงาน สมัครฝึกงาน หรือสมัครสหกิจ เราได้รวบรวมบริษัทชั้นนำมาไว้ภายในงานนี้',
+      "schedule": [
+            {
+                  "topic":"",
+                  "date":"วันที่ 1 – 2 กุมภาพันธ์ 2560",
+                  "time":"9.00 – 16.00 น."
+            }
+      ],
+      "location": "อาคารปฏิบัติการวิศวกรรม 2 หรือตึก ECC ชั้น 8 ",
+      "register": "",                                                                                                                                                                                           
+      "calendar": "",
+      "contact": 
+            {
+                  "website": "https://goo.gl/qvDMCj",
+                  "email": "",
+                  "tel": [
+                        {
+                              "name": "",
+                              "number": ""
+                        }
+                  ]
+            },
+      "share": {
+                  "facebook": "https://www.facebook.com/sharer/sharer.php?u=https://goo.gl/qvDMCj&display=popup",
+                  "googleplus": "https://plus.google.com/share?url=https://goo.gl/qvDMCj",
+                  "line": "http://line.me/R/msg/text/?https://goo.gl/qvDMCj",
+                  "twitter": "https://twitter.com/intent/tweet?url=https://goo.gl/qvDMCj",
+                  "email": {
+                        "subject": 'CE Smart Career',
+                        "body": "https://goo.gl/qvDMCj"
+                  }
+            }
       }
 ]
 
