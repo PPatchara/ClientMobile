d376f78a47bfd47fcb04496cecfd8b169123095f
PATCHARA PATTANAKIMHUN <s6070091@kmitl.ac.th>
2017-07-19 21:24:30 +0700

    edit generate key

--- helpers.js
+++ helpers.js
@@ -10,11 +10,12 @@ function uniqueID() {
 }
 
 function generateKey() {
-  return Math.random().toString(16).slice(-4);
+  return Math.random().toString().slice(-4);
 }
 
 function generateRenewCode() {
-  return Math.floor(1000 + Math.random() * 9000).toString();
+  return Math.random().toString().slice(-3);
+  // return Math.floor(1000 + Math.random() * 9000).toString();
 }
 
 module.exports = {
